e3705780e8c95f08a5cfccb7c343314c71fa1ab4
stijn-rgb <stijn@svdbperformance.nl>
2026-04-20 15:43:47 +0200

    checkup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Beleggen from './pages/Beleggen'
 import Sparen from './pages/Sparen'
@@ -9,7 +10,6 @@ import Verzekeringen from './pages/Verzekeringen'
 import Tools from './pages/Tools'
 import Blog from './pages/Blog'
 import OverOns from './pages/OverOns'
-import ScrollToTop from './components/ScrollToTop'
 import BeginnenMetBeleggen from './pages/articles/BeginnenMetBeleggen'
 import CompoundCalculator from './pages/tools/CompoundCalculator'
 import VrijheidCalculator from './pages/tools/VrijheidCalculator'
@@ -20,25 +20,28 @@ import BrokerVergelijker from './pages/tools/BrokerVergelijker'
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/beleggen" element={<Beleggen />} />
-        <Route path="/sparen" element={<Sparen />} />
-        <Route path="/extra-inkomen" element={<ExtraInkomen />} />
-        <Route path="/schulden" element={<Schulden />} />
-        <Route path="/verzekeringen" element={<Verzekeringen />} />
-        <Route path="/tools" element={<Tools />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/over-ons" element={<OverOns />} />
-        <Route path="/tools/compound-calculator" element={<CompoundCalculator />} />
-        <Route path="/tools/vrijheid-calculator" element={<VrijheidCalculator />} />
-        <Route path="/tools/schulden-calculator" element={<SchuldenCalculator />} />
-        <Route path="/tools/budget-planner" element={<BudgetPlanner />} />
-        <Route path="/tools/pensioen-calculator" element={<PensioenCalculator />} />
-        <Route path="/tools/broker-vergelijker" element={<BrokerVergelijker />} />
-        <Route path="/beleggen/beginnen-met-beleggen" element={<BeginnenMetBeleggen />} />
-      </Routes>
-    </Layout>
+    <>
+      <ScrollToTop />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/beleggen" element={<Beleggen />} />
+          <Route path="/sparen" element={<Sparen />} />
+          <Route path="/extra-inkomen" element={<ExtraInkomen />} />
+          <Route path="/schulden" element={<Schulden />} />
+          <Route path="/verzekeringen" element={<Verzekeringen />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/over-ons" element={<OverOns />} />
+          <Route path="/tools/compound-calculator" element={<CompoundCalculator />} />
+          <Route path="/tools/vrijheid-calculator" element={<VrijheidCalculator />} />
+          <Route path="/tools/schulden-calculator" element={<SchuldenCalculator />} />
+          <Route path="/tools/budget-planner" element={<BudgetPlanner />} />
+          <Route path="/tools/pensioen-calculator" element={<PensioenCalculator />} />
+          <Route path="/tools/broker-vergelijker" element={<BrokerVergelijker />} />
+          <Route path="/beleggen/beginnen-met-beleggen" element={<BeginnenMetBeleggen />} />
+        </Routes>
+      </Layout>
+    </>
   )
 }
